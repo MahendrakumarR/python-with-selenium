6dbdbd876f9efd9fd968e03328689187b47e8117
MahendraN <mahendraramalingam8056@gmail.com>
2024-08-20 19:45:29 +0530

    Update

--- day 2.py
+++ day 2.py
@@ -27,3 +27,35 @@ print("Employee Phone Number:",emp_Phone_No)
 print("Employee Salary:",emp_Salary)
 print("Employee Gender:",emp_Gender)
 print("Employee City:",emp_City)
+
+"""
+QUESTION 2:
+-------------
+Description: Using input() method get the below details
+        studentId
+        studentName
+        Mark_1
+        Mark_2
+        Mark_3
+        Mark_4
+        Mark_5
+        :Find the total and average of marks
+
+"""
+studentId    = int(input("\nEnter Student Id:"))
+studentName  = input("Enter Student Name:")
+Mark_1       = int(input("Enter Mark_1:"))
+Mark_2       = int(input("Enter Mark_2:"))
+Mark_3       = int(input("Enter Mark_3:"))
+Mark_4       = int(input("Enter Mark_4:"))
+Mark_5       = int(input("Enter Mark_5:"))
+
+print("\nStudent Details")
+print("=================")
+print("Student Id is:",studentId)
+print("Student Name is:",studentName)
+print("Student Mark_1 is:",Mark_1)
+print("Student Mark_2 is:",Mark_2)
+print("Student Mark_3 is:",Mark_3)
+print("Student Mark_4 is:",Mark_4)
+print("Student Mark_5 is:",Mark_5)
