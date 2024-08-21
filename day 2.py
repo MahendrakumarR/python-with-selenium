dd7655b5ef4f779d0dd070c7684bb9df26098ee4
MahendraN <mahendraramalingam8056@gmail.com>
2024-08-21 20:01:12 +0530

    Update

--- day 2.py
+++ day 2.py
@@ -11,13 +11,13 @@ Description: Using datatype intialize the below details
              emp_City
 """
 """
-emp_Id          = int(input("Enter Employee id:"))
-emp_Name        = input("Enter Employee Name:")
-emp_Email       = input("Enter Employee Email id:")
-emp_Phone_No    = int(input("Enter Employee Phone Number:"))
-emp_Salary      = int(input("Enter Employee Salary:"))
-emp_Gender      = input("Enter Employee Gender:")
-emp_City        = input("Enter Employee City:")
+emp_Id          = 1201
+emp_Name        = "MahendraN"
+emp_Email       = "mahe@gmail.com"
+emp_Phone_No    = 0987654321
+emp_Salary      = 23021.40
+emp_Gender      = "Male"
+emp_City        = "Chennai"
 
 print("\nEmployee Details")
 print("================")
@@ -45,6 +45,7 @@ Description: Using input() method get the below details
         :Find the total and average of marks
 
 """
+"""
 studentId    = int(input("\nEnter Student Id:"))
 studentName  = input("Enter Student Name:")
 Mark_1       = int(input("Enter Mark_1:"))
@@ -55,7 +56,7 @@ Mark_5       = int(input("Enter Mark_5:"))
 total        = Mark_1 + Mark_2 + Mark_3 + Mark_4 + Mark_5
 average      = Mark_1 + Mark_2 + Mark_3 + Mark_4 + Mark_5 / 5
 
-print("\nStudent Details")
+print("\nStudent Mark Details")
 print("=================")
 print("Student Id is:",studentId)
 print("Student Name is:",studentName)
@@ -66,3 +67,71 @@ print("Student Mark_4 is:",Mark_4)
 print("Student Mark_5 is:",Mark_5)
 print("Total:",total)
 print("Average is:",average)
+
+"""
+"""
+QUESTION 3:
+------------
+Description: Using input() method get the below details
+        student_Id
+        student_Name
+        student_Email
+        student_Phoneno
+        student_Dept
+        student_Gender
+        student_City
+
+"""
+"""
+student_Id = int(input("Enter Student ID: "))
+student_Name = input("Enter Student Name: ")
+student_Email = input("Enter Student Email: ")
+student_Phoneno = int(input("Enter Student Phone Number: "))
+student_Dept = input("Enter Student Department: ")
+student_Gender = input("Enter Student Gender: ")
+student_City = input("Enter Student City: ")
+
+print("\nStudent Details")
+print("===============")
+print("Student Id:",student_Id)
+print("Student Name:",student_Name)
+print("Student Email id:",student_Email)
+print("Student Phone Number:",student_Phoneno)
+print("Student Department:",student_Dept)
+print("Student Gender:",student_Gender)
+print("Student City:",student_City)
+
+"""
+"""
+QUESTION 4:
+------------
+Description: Using datatype intialize the below details
+        clg_Id
+        clg_Name
+        clg_Email
+        clg_Phone_No
+        clg_Code
+        clg_Dept_Count
+        clg_Address
+
+"""
+"""
+clg_Id         = 121
+clg_Name       = "Sri Vasavi College"
+clg_Email      = "info@svc.edu.in"
+clg_Phone_No   = 9876543210
+clg_Code       = 1001
+clg_Dept_Count = 13
+clg_Address    = "Erode"
+
+print("\n College Details")
+print("==================")
+print("College Id is:",clg_Id)
+print("College Name is:",clg_Name)
+print("College Email is:",clg_Email)
+print("College Phone number is:",clg_Phone_No)
+print("College Code is:",clg_Code)
+print("College Department Count is:",clg_Dept_Count)
+print("College Address is:",clg_Address)
+
+"""
