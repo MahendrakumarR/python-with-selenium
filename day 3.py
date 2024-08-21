6ab237a58b6a1f2e8f43bb3fd6065aba532ad6ee
MahendraN <mahendraramalingam8056@gmail.com>
2024-08-21 20:27:46 +0530

    Update

---
+++ day 3.py
@@ -0,0 +1,31 @@
+"""
+QUESTION 1:
+-----------
+Description: Find the length of the below string
+
+              String_1: GreensTechnology
+              String_2: Python Programming
+              String_3: s e l e n i u m
+              String_4: 9876543210
+              String_5: Hi welcome to the world of programs
+"""
+# Strings provided in the question
+String_1 = "GreensTechnology"
+String_2 = "Python Programming"
+String_3 = "s e l e n i u m"
+String_4 = "9876543210"
+String_5 = "Hi welcome to the world of programs"
+
+# Finding the length of each string using len() function
+length_String_1 = len(String_1)
+length_String_2 = len(String_2)
+length_String_3 = len(String_3)
+length_String_4 = len(String_4)
+length_String_5 = len(String_5)
+
+# Displaying the lengths of the strings
+print(f"Length of String_1 ('{String_1}') is: {length_String_1}")
+print(f"Length of String_2 ('{String_2}') is: {length_String_2}")
+print(f"Length of String_3 ('{String_3}') is: {length_String_3}")
+print(f"Length of String_4 ('{String_4}') is: {length_String_4}")
+print(f"Length of String_5 ('{String_5}') is: {length_String_5}")
