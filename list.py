bb761467d8f49440c84a985f464693806d2c2067
MahendraN <mahendraramalingam8056@gmail.com>
2024-08-26 20:38:23 +0530

    Update

---
+++ list.py
@@ -0,0 +1,24 @@
+# list
+
+# append()
+a = [1,2,3,4]
+a.append(5)
+print(a)         # [1, 2, 3, 4, 5]
+
+# max()
+a = [1,2,3,4]
+v = max(a)
+print(v)         # 4
+
+
+# min()
+a = [1,2,3,4]
+v = min(a)
+print(v)         # 1
+
+# extend()
+
+# append()
+a = [1,2,3,4]
+a.extend([5,6,7,8])
+print(a) 
