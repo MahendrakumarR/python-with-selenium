9a9d70a5782894fcc2ba6c3f6caf779648dbeca1
MahendraN <mahendraramalingam8056@gmail.com>
2024-09-08 20:04:41 +0530

    Update

--- day 6.py
+++ day 6.py
@@ -166,3 +166,211 @@ print(output_2)             # elco
 print(output_3)             # to
 print(output_4)             # python
 print(output_5)             # to python class
+
+"""
+QUESTION 5:
+-----------
+Description:Find whether the string python is present or not using 'if'
+
+Input = Programming languages are c,c++,Java and Python 
+   
+   if (s.find('Python') != -1):
+  
+     print("Contains substring 'Python'")
+   
+else:
+  
+     print("Doesn't contain substring")
+"""
+s = "Programming languages are c,c++,Java and Python"
+if (s.find('Python') != -1): 
+    print("Contains substring 'Python'")                # Contains substring 'Python'
+else:
+    print("Doesn't contain substring")
+
+"""
+QUESTION 6:
+-----------
+
+QUESTION 6.1:
+-------------
+Description: Given String as "Welcome to Python class" and split it by space.
+
+Example:
+--------
+Input = "Welcome to Python class"
+
+Output:
+-------
+Welcome
+to
+Python
+class
+
+"""
+splite_ = "Welcome to Python class"
+sp_v = splite_.split()
+for i in sp_v:
+    print(i)                        # Welcome | to | python | class (top to bottom)
+
+"""
+QUESTION 6.2:
+-------------
+Description: Given String as "Welcome to python class" and split it by l 
+
+Example:
+--------
+Input = "Welcome to python class"
+Output:
+-------
+Welcome 
+to python class
+
+"""
+splite_1 = "Welcome to Python class"
+sp_v2 = splite_1.split(" ",1)           # first space after all print
+for i in sp_v2:
+    print(i)                            # Wlecome | to python class
+                                        
+"""
+QUESTION 7:
+-----------
+Description:Find the count of word "is"
+
+Input: "Python is awesome and it is dynamic language"
+
+output:
+------
+2   
+
+"""
+cou = "Python is awesome and it is dynamic language"
+print(cou.count('is'))                                # 2
+
+"""
+QUESTION 8:
+-------------
+Description:Get the input from the user and find the count of character 'o'
+
+"""
+# user = input("Enter the String:")         # hello everyone
+# print(user.count('o'))                    # 2
+
+"""
+QUESTION 9:
+----------
+
+Description: Given String as "Welcome" and count the number of consonants and vowels
+
+Example:
+---------
+Input  = Welcome
+
+output:
+-------
+vowels    = 3
+consonants = 4
+
+"""
+str_ = "Welcome"
+
+vowels = 0
+consonants = 0
+
+vowels_ = "aeiouAEIOU"
+
+for char in str_:
+    if char.isalpha():
+        if char in vowels_:
+            vowels += 1    
+        else:
+            consonants += 1
+            
+print(f"Vowels : {vowels}")          # Vowels : 3
+print(f"Consonants : {consonants}")  # Consonants : 4
+
+"""
+QUESTION 10:
+------------
+Description : Get two input strings from user and Compare 
+
+"""
+#str_1 = input("Enter First String:")        # python
+#str_2 = input("Enter second String:")       # Python
+#print(str_1 == str_2)                       # False
+
+"""
+QUESTION 11:
+------------
+QUESTION 11.1
+-------------
+Description: Given String as "Welcome to Python class" and verify whether the given string startsWith, endsWith
+
+"""
+given_str = "Welcome to Python class"
+print(given_str.startswith("Welcome"))      # True
+print(given_str.startswith("hi"))           # False
+print(given_str.endswith("class"))          # True
+print(given_str.endswith("python"))         # False
+
+"""
+QUESTION 12:
+------------
+
+QUESTION 12.1
+-------------
+Description:Get the input from the user and print that word in lowercase, uppercase, capital
+
+"""
+#user_1 = input("Enter the String in Uppercase:")     # HELLO
+#user_2 = input("Enter the String in Lowercase:")     # welcome
+#user_3 = input("Enter the String in smallletters:")  # mahendran
+#print(user_1.lower())                                # hello
+#print(user_2.upper())                                # WELCOME
+#print(user_3.capitalize())                           # Mahendran
+
+"""
+QUESTION 13:
+------------
+QUESTION 13.1:
+--------------
+Description:Remove the unwanted spaces from the given string
+
+Example:
+--------
+Input  = "      Incredible India        "
+Output = Incredible India
+
+"""
+input = "   Incredible India   "
+val = input.strip()                  # both left and right side space remove
+val_r = input.rstrip()               # right side space only remove
+val_l = input.lstrip()               # left side space only remove
+print(val)                           # Incredible India
+print(val_r)                         #    Incredible India
+print(val_l)                         # Incredible India
+
+"""
+Question 14:
+------------
+Description:Check whether the given inputs are isnumeric,isdigit and isdecimal
+
+Inputs:
+-------
+s1 = "45"
+s2 = "Â¾"
+s3 = "4Â¾"
+s4 = "Â²"
+s5 = "1/4"
+
+"""
+s1 = "45"
+s2 = "Â¾"
+s3 = "4Â¾"
+s4 = "Â²"
+s5 = "1/4"
+print(s1.isalpha())             # False
+print(s2.isnumeric())           # False
+print(s3.isalnum())             # True
+print(s4.isdecimal())           # False
+print(s5.isdigit())             # False
